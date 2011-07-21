612ea214ea18b300d61bf6aa853e88265bb96868
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-07-21 18:49:53 +0200

    Avoid MFC as dependency
    It is not really a dependency to begin with :-)
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- DeviceAdapters/PCO_Generic/StdAfx.h
+++ DeviceAdapters/PCO_Generic/StdAfx.h
@@ -14,13 +14,7 @@
 
 #define VC_EXTRALEAN		// Selten verwendete Teile der Windows-Header nicht einbinden
 
-#include <afxwin.h>         // MFC-Kern- und -Standardkomponenten
-#include <afxext.h>         // MFC-Erweiterungen
-#include <afxdtctl.h>		// MFC-Unterstützung für allgemeine Steuerelemente von Internet Explorer 4
-#ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>			// MFC-Unterstützung für gängige Windows-Steuerelemente
-#endif // _AFX_NO_AFXCMN_SUPPORT
-
+#include <windows.h>
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ fügt unmittelbar vor der vorhergehenden Zeile zusätzliche Deklarationen ein.

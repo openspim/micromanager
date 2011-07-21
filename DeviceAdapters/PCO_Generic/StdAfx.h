535f834551378df2e1318c7891c1332c87c23a24
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-07-21 18:49:53 +0200

    Avoid MFC as dependency
    It is not really a dependency to begin with :-)
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ DeviceAdapters/PCO_Generic/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : Include-Datei für Standard-System-Include-Dateien,
+//  oder projektspezifische Include-Dateien, die häufig benutzt, aber
+//      in unregelmäßigen Abständen geändert werden.
+//
+
+#if !defined(AFX_STDAFX_H__7132FEB0_2F9E_4E17_86A4_FD93C7A50220__INCLUDED_)
+#define AFX_STDAFX_H__7132FEB0_2F9E_4E17_86A4_FD93C7A50220__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WINVER 0x0501
+
+#define VC_EXTRALEAN		// Selten verwendete Teile der Windows-Header nicht einbinden
+
+#include <windows.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ fügt unmittelbar vor der vorhergehenden Zeile zusätzliche Deklarationen ein.
+
+#endif // !defined(AFX_STDAFX_H__7132FEB0_2F9E_4E17_86A4_FD93C7A50220__INCLUDED_)

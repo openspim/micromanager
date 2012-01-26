3e2e8727c5fb703fc89e94658bf2bba68468e6d1
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-01-26 10:39:40 -0600

    Modify acqEngine's build.bat so it can compile with Fiji in dist/
    ... as we introduced for the OpenSPIM project.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- acqEngine/build.bat
+++ acqEngine/build.bat
@@ -3,11 +3,8 @@ if exist build rmdir build /s /q
 mkdir classes
 mkdir build
 xcopy /E /y src classes\
-"%JAVA_HOME%\bin\java" -cp ../../3rdpartypublic/classext/clojure.jar;../MMCoreJ_wrap/MMCoreJ.jar;../../3rdpartypublic/classext/ij.jar;../mmstudio/MMJ_.jar;../../3rdpartypublic/classext/bsh-2.0b4.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
+"%JAVA_HOME%\bin\java" -cp ../bin_Win32/plugins/clojure.jar;../bin_Win32/plugins/MMCoreJ.jar;../bin_Win32/jars/ij-1*.jar;../bin_Win32/plugins/MMJ_.jar;../bin_Win32/plugins/bsh-2.0b4.jar;./src -Dclojure.compile.path=classes clojure.lang.Compile org.micromanager.acq-engine
 xcopy /E /y src build\
 xcopy classes\org\micromanager\AcquisitionEngine2010.class build\org\micromanager
 "%JAVA_HOME%\bin\jar" cf MMAcqEngine.jar -C build\ .    
-copy /Y MMAcqEngine.jar ..\bin_Win32\
-copy /Y MMAcqEngine.jar ..\bin_x64\
-copy /Y MMAcqEngine.jar ..\Install_Win32\micro-manager\plugins\Micro-Manager"
-copy /Y MMAcqEngine.jar ..\Install_x64\micro-manager\plugins\Micro-Manager"
+copy /Y MMAcqEngine.jar ..\bin_Win32\plugins

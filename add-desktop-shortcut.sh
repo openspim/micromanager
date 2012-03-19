e05dad829f8f8087c05aa99905c01509eb2efd92
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-03-19 16:21:00 -0500

    fixup! profile.d/ script: Always reinstate the desktop icon

--- add-desktop-shortcut.sh
+++ add-desktop-shortcut.sh
@@ -2,12 +2,10 @@
 
 
 test -f "$USERPROFILE/Desktop/OpenSPIM Dev Env.lnk" || {
-	(cd /share/msysGit && make) || exit
-	ico="$(cd "$(dirname "$0")" && pwd -W | sed -e 's/\\/\//g' -e 's/$/mm-spim.ico/')"
-	tcl=/tmp/tmp.$$.tcl
+	ico="$(cd "$(dirname "$0")" && pwd -W | sed -e 's/\\/\//g' -e 's/$/\/mm-spim.ico/')"
+	tcl=/share/msysGit/tmp.$$.tcl
 	sed -e 's/msysGit.lnk/OpenSPIM Dev Env.lnk/' \
 		-e "s|\$resDirectory/msysgitlogo.ico|$ico|" \
-		-e '/exec make/d' \
 	< /share/msysGit/add-shortcut.tcl > $tcl
 	$tcl d
 	rm $tcl

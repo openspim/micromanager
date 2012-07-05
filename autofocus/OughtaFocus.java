e22552fc80aa210077041d7db14f2c84c0ab3ace
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2012-07-05 16:54:05 -0500

    Started porting MicroManager and plugins to commons math 3.
    We really need to condense these down to one of two versions, and there's
    no sense using version 2 when 3 is available.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- autofocus/OughtaFocus.java
+++ autofocus/OughtaFocus.java
@@ -35,7 +35,6 @@ import mmcorej.CMMCore;
 import mmcorej.Configuration;
 import mmcorej.TaggedImage;
 
-import org.apache.commons.math.FunctionEvaluationException;
 import org.apache.commons.math.analysis.UnivariateRealFunction;
 import org.apache.commons.math.optimization.GoalType;
 import org.apache.commons.math.optimization.univariate.BrentOptimizer;
@@ -160,20 +159,19 @@ public class OughtaFocus extends AutofocusBase implements org.micromanager.api.A
 
    private double runAutofocusAlgorithm() throws Exception {
       UnivariateRealFunction scoreFun = new UnivariateRealFunction() {
-         public double value(double d) throws FunctionEvaluationException {
+         public double value(double d) {
             return measureFocusScore(d);
          }
       };
-      BrentOptimizer brentOptimizer = new BrentOptimizer();
-      brentOptimizer.setAbsoluteAccuracy(tolerance);
+      BrentOptimizer brentOptimizer = new BrentOptimizer(tolerance,1e-11);
       imageCount_ = 0;
 
       CMMCore core = app_.getMMCore();
       double z = core.getPosition(core.getFocusDevice());
       startZUm_ = z;
       getCurrentFocusScore();
-      double zResult = brentOptimizer.optimize(scoreFun, GoalType.MAXIMIZE, z - searchRange / 2, z + searchRange / 2);
-      ReportingUtils.logMessage("OughtaFocus Iterations: " + brentOptimizer.getIterationCount() +
+      double zResult = brentOptimizer.optimize(400000000, scoreFun, GoalType.MAXIMIZE, z - searchRange / 2, z + searchRange / 2);
+      ReportingUtils.logMessage("OughtaFocus Iterations: " + brentOptimizer.getEvaluations() +
             ", z=" + TextUtils.FMT2.format(zResult) +
             ", dz=" + TextUtils.FMT2.format(zResult - startZUm_) +
             ", t=" + (System.currentTimeMillis() - startTimeMs_));

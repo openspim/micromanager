73857f01352c8fcaab062144efc8e521ee077d02
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-10-14 19:07:20 -0500

    Startup script: make sure jvisualvm is in the PATH
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- openspim.sh
+++ openspim.sh
@@ -108,6 +108,15 @@ export JAVA_HOME="\$(cd "$FIJI_JAVA_HOME" && pwd -W)"
 exec "$(pwd)/3rdpartypublic/apache-ant-1.6.5/bin/ant" "\$@"
 EOF
 	 fi &&
+	 if ! test -x "$HOME/bin/jvisualvm"
+	 then
+		cat > "$HOME/bin/jvisualvm" << EOF
+#!/bin/sh
+
+export JAVA_HOME="\$(cd "$FIJI_JAVA_HOME" && pwd -W)"
+exec "\$JAVA_HOME/bin/jvisualvm" "\$@"
+EOF
+	 fi &&
 	 (cd micromanager &&
 	  if ! test -f bin_Win32/ImageJ-win32.exe
 	  then

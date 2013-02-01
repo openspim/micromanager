34038ef9e77012cd77570f3536e61381ceb2470c
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-02-01 09:50:45 -0600

    Prepare add-desktop-shortcut to run outside msysGit
    We want to switch the development environment from an msysGit-based one to a Git for Windows one. (msysGit is the development environment for Git for Windows, featuring a GNU C/C++ compiler, and building Git from scratch, which we do not need here.)
    
    The original idea was that we need the C compiler to build Fiji, which was true until the Fiji launcher migrated to ImageJ2. But we do not even need to build Fiji any longer; Jenkins does that for us.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- add-desktop-shortcut.sh
+++ add-desktop-shortcut.sh
@@ -3,10 +3,27 @@
 
 test -f "$USERPROFILE/Desktop/OpenSPIM Dev Env.lnk" || {
 	ico="$(cd "$(dirname "$0")" && pwd -W | sed -e 's/\\/\//g' -e 's/$/\/mm-spim.ico/')"
-	tcl=/share/msysGit/tmp.$$.tcl
+	BIN=/bin
+	test -x $BIN/add-shortcut.tcl || {
+		BIN=/share/msysGit
+		test -x $BIN/add-shortcut.tcl || {
+			echo "Skipping shortcut creation" >&2
+			exit 1
+		}
+	}
+	tcl=$BIN/tmp.$$.tcl
 	sed -e 's/msysGit.lnk/OpenSPIM Dev Env.lnk/' \
 		-e "s|\$resDirectory/msysgitlogo.ico|$ico|" \
-	< /share/msysGit/add-shortcut.tcl > $tcl
-	$tcl d
+	< $BIN/add-shortcut.tcl |
+	case $BIN in
+	/share/msysGit)
+		cat
+		;;
+	*)
+		sed -e 's/\[file dirname \(\[file dirname \[pwd\]\]\)\]/\1/' \
+			-e 's/exec make//'
+		;;
+	esac > $tcl
+	tclsh $tcl d
 	rm $tcl
 }

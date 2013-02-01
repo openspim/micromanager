7496880d0bfa81cddcbd044c9d4720fbcd284e63
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-02-01 10:57:44 -0600

    Adapt build.sh for the switch away from dist/
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-FIJI_JAVA_HOME= &&
-for d in /src/fiji/java/win32/*
-do
-	if test -z "$FIJI_JAVA_HOME" || test "$d" -nt "$FIJI_JAVA_HOME"
-	then
-		FIJI_JAVA_HOME="$d"
-	fi
-done &&
 : "Fiji's Java is located at $FIJI_JAVA_HOME"
 
 cd "$(dirname "$0")" &&
 vcexpress.sh MMCoreJ_wrap/MMCoreJ_wrap.sln //build Debug //project mmstudio &&
-PATH=$FIJI_JAVA_HOME/bin:$PATH ./dist/ImageJ.exe --build &&
+PATH=$FIJI_JAVA_HOME/bin:$PATH ./bin_Win32/ImageJ-win32.exe --build &&
 start mmstudio/Debug/BuildLog.htm &

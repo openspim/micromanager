46132e161f4d80c26f9ed2fd777d23cd419096f5
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-04-25 18:49:22 -0500

    Vastly improved build-openspim.sh
    I know eventually I'll have to un-sillify it (professionalism and such),
    but until that day, it shall contain emoticons as prescribed.
    
    Anyway, it now multitasks, building MMCore/mmstudio, then the DALs (Cube
    and OpenSPIM (to be renamed Picard), for now), and finally the
    Micro-Manager plugins (which don't seem to be compiling to the correct
    place; this will require investigation.)
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- build-openspim.sh
+++ build-openspim.sh
@@ -1,15 +1,56 @@
 #!/bin/sh
 
-# TODO: Set all the correct environment variables; check the VS2010 bat.
+die() {
+	echo $1;
+	exit 1;
+}
+
+banner() {
+	echo;
+	echo "----==== $1 ====----";
+	echo;
+}
+
+bannerdie() {
+	echo;
+	echo "----==== $1 ====----";
+	echo;
+	exit 1;
+}
+
+banner "FINDING MY MARBLES D:"
+
+# TODO: Set all the correct environment variables; check the VS2010 bat?
 dotnetfwdir=$(reg query HKLM\\SOFTWARE\\Microsoft\\VisualStudio\\SxS\\VC7 //v FrameworkDir32 | grep 'FrameworkDir32' | sed -r -e 's/\s*FrameworkDir32\s*REG_SZ\s*([^\s]*)/\1/g');
 dotnetfwver=$(reg query HKLM\\SOFTWARE\\Microsoft\\VisualStudio\\SxS\\VC7 //v FrameworkVer32 | grep 'FrameworkVer32' | sed -r -e 's/\s*FrameworkVer32\s*REG_SZ\s*([^\s]*)/\1/g');
 
 msbuild="$dotnetfwdir$dotnetfwver\\msbuild.exe";
 
-if ! test -a $msbuild;
-then
-	echo "Couldn't find msbuild.exe in $msbuild. No .NET framework?";
-	exit 1;
-fi &&
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:OpenSPIM\;CoherentCube //fileLogger //detailedsummary
+test -a $msbuild || die "Couldn't find msbuild.exe ($msbuild). No .NET framework?";
 
+echo "Found msbuild.exe at $msbuild."
+
+# TODO: Is this recorded anywhere definitive?
+ant="$(pwd)/../3rdpartypublic/apache-ant-1.6.5/bin/ant"
+
+test -a $ant || die "Couldn't find Apache Ant at $ant. Did you pull 3rdpartypublic?";
+
+echo "Found ant at $ant."
+
+banner "BUILDING MMCORE & MMSTUDIO"
+
+$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:MMCore\;MMCoreJ_wrap\;mmstudio //fileLogger1 //verbosity:minimal || bannerdie "FAILED TO BUILD MMCORE/MMSTUDIO! :(";
+
+test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+
+banner "BUILDING DEVICE ADAPTERS"
+
+$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:DemoCamera\;TIScam\;QCam\;OpenSPIM\;SerialManager\;CoherentCube //fileLogger2 //verbosity:minimal || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+
+test "$(grep -c '^Build FAILED\.$' msbuild2.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+
+banner "BUILDING MICRO-MANAGER PLUGINS"
+
+$ant -quiet -buildfile "plugins/build.xml" clean compile build || bannerdie "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(";
+
+banner "DONE! :D"

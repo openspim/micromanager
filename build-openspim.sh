475ed99eeda3ea97fbf86144fa26821d5a7ab968
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-05-02 18:05:53 -0500

    fixup! Vastly improved build-openspim.sh
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- build-openspim.sh
+++ build-openspim.sh
@@ -11,6 +11,13 @@ banner() {
 	echo;
 }
 
+bannerdie() {
+	echo;
+	echo "----==== $1 ====----";
+	echo;
+	exit 1;
+}
+
 banner "FINDING MY MARBLES D:"
 
 # TODO: Set all the correct environment variables; check the VS2010 bat?
@@ -32,14 +39,18 @@ echo "Found ant at $ant."
 
 banner "BUILDING MMCORE & MMSTUDIO"
 
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:MMCore\;MMCoreJ_wrap\;mmstudio //fileLogger2 //verbosity:minimal || banner "FAILED TO BUILD MMCORE/MMSTUDIO! :(" | die;
+$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:MMCore\;MMCoreJ_wrap\;mmstudio //fileLogger1 //verbosity:minimal || bannerdie "FAILED TO BUILD MMCORE/MMSTUDIO! :(";
+
+test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
 
 banner "BUILDING DEVICE ADAPTERS"
 
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:OpenSPIM\;CoherentCube //fileLogger1 //verbosity:minimal || banner "FAILED TO BUILD DEVICE ADAPTERS! :(" | die;
+$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:DemoCamera\;TIScam\;QCam\;OpenSPIM\;SerialManager\;CoherentCube //fileLogger2 //verbosity:minimal || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+
+test "$(grep -c '^Build FAILED\.$' msbuild2.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
 
 banner "BUILDING MICRO-MANAGER PLUGINS"
 
-$ant -quiet -buildfile "plugins/build.xml" clean compile build || banner "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(" | die;
+$ant -quiet -buildfile "plugins/build.xml" clean compile build || bannerdie "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(";
 
 banner "DONE! :D"

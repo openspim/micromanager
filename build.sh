2b18d346d6b987bec1e0455512b778d9105391a8
Luke Stuyvenberg <stuyvenberg@wisc.edu>
2013-10-14 18:56:38 -0500

    Update build system to MM's new model
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>

--- build.sh
+++ build.sh
@@ -27,7 +27,8 @@ registry_query() {
 platform="Win32"
 config="Release"
 target=
-slnver=
+quiet="-quiet"
+verb="//verbosity:minimal"
 
 for arg;
 do
@@ -41,11 +42,9 @@ do
 	--rebuild)
 		target=":REBUILD";
 		;;
-	--vc100)
-		slnver="_v10";
-		;;
-	--vc90)
-		slnver="_v9";
+	--verbose)
+		quiet="";
+		verb="";
 		;;
 	*)
 		;;
@@ -56,13 +55,20 @@ banner "PREPARING BUILD ENVIRONMENT";
 
 sh prepare-for-build.sh;
 
+if ! [ "${JAVA_HOME}" ];
+then
+	export JAVA_HOME="$(pwd)/bin_Win32/java/win32/jdk1.6.0_24";
+
+	if ! test -f "$JAVA_HOME/include/jni.h";
+	then
+		die "Couldn't locate JDK: JAVA_HOME not set and prepare-for-build.sh may have failed...";
+	fi;
+fi;
+
 banner "FINDING MY MARBLES D:";
 
 dotnetfwdir="$(registry_query "HKEY_LOCAL_MACHINE\\\\SOFTWARE\\\\Microsoft\\\\VisualStudio\\\\SxS\\\\VC7" "FrameworkDir32")"
 dotnetfwver="$(registry_query "HKEY_LOCAL_MACHINE\\\\SOFTWARE\\\\Microsoft\\\\VisualStudio\\\\SxS\\\\VC7" "FrameworkVer32")"
-
-test -z "$slnver" && test "$(echo "$dotnetfwver" | head -c2)" = "v4" && slnver="_v10";
-test "$slnver" = "_v9" && slnver='' dotnetfwver='v3.5';
 
 msbuild="$dotnetfwdir$dotnetfwver\\msbuild.exe";
 
@@ -77,22 +83,47 @@ test -x $ant || die "Couldn't find Apache Ant at $ant. Did you pull 3rdpartypubl
 
 echo "Found ant at $ant.";
 
-banner "${target:+RE}BUILDING MMCORE & MMSTUDIO IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
+if [ "${target}" ];
+then
+	banner "CLEANING OUTPUT DIRECTORY"
 
-# For some reason, you can't specify Build as a target -- you must leave off the target specifier entirely.
+	$ant ${quiet} -Dmm.architecture=${platform} -buildfile "build.xml" clean-all unstage-all || bannerdie "COULDN'T CLEAN OUTPUT DIRECTORY! :(";
+fi;
 
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap${slnver}.sln /property:Configuration=$config /property:Platform=$platform /target:MMCore${target}\;MMCoreJ_wrap${target}\;mmstudio${target} //fileLogger1 //verbosity:minimal && test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD MMCORE/MMSTUDIO! :(";
+banner "${target:+RE}BUILDING MMCORE IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
+
+$msbuild micromanager.sln /property:Configuration=$config /property:Platform=$platform /target:MMCore${target} //fileLogger1 ${verb} && test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD MMCORE! :(";
+
+banner "${target:+RE}BUILDING MMCOREJ_WRAP (C++) IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
+
+$msbuild micromanager.sln /property:Configuration=$config /property:Platform=$platform /target:MMCoreJ_wrap${target} //fileLogger1 ${verb} && test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD MMCOREJ_WRAP (C++)! :(";
 
 banner "${target:+RE}BUILDING DEVICE ADAPTERS IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
 
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap${slnver}.sln /property:Configuration=$config /property:Platform=$platform /target:DemoCamera${target}\;PicardStage${target}\;SerialManager${target}\;CoherentCube${target} //fileLogger2 //verbosity:minimal && test "$(grep -c '^Build FAILED\.$' msbuild2.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+$msbuild micromanager.sln /property:Configuration=$config /property:Platform=$platform /target:DemoCamera${target}\;PicardStage${target}\;SerialManager${target}\;CoherentCube${target} //fileLogger2 ${verb} && test "$(grep -c '^Build FAILED\.$' msbuild2.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
 
-banner "BUILDING MICRO-MANAGER ACQUISITION ENGINE";
+banner "${target:+RE}BUILDING MMCOREJ_WRAP (JAVA) IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
 
-$ant -quiet -buildfile "acqEngine/build.xml" ${target:+clean }compile build || bannerdie "FAILED TO BUILD ACQUISITION ENGINE! :(";
+$ant ${quiet} -Dmm.architecture=${platform} -buildfile "MMCoreJ_wrap/build.xml" ${target:+clean }jar || bannerdie "FAILED TO BUILD MMCOREJ_WRAP (JAVA)! :(";
 
-banner "BUILDING MICRO-MANAGER PLUGINS";
+banner "${target:+RE}BUILDING MMSTUDIO IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
 
-$ant -quiet -buildfile "plugins/build.xml" ${target:+clean }compile build || bannerdie "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(";
+$ant ${quiet} -Dmm.architecture=${platform} -buildfile "mmstudio/build.xml" ${target:+clean }jar || bannerdie "FAILED TO BUILD MMSTUDIO! :(";
+
+banner "${target:+RE}BUILDING MICRO-MANAGER ACQUISITION ENGINE";
+
+$ant ${quiet} -Dmm.architecture=${platform} -buildfile "acqEngine/build.xml" ${target:+clean }jar || bannerdie "FAILED TO BUILD ACQUISITION ENGINE! :(";
+
+banner "${target:+RE}BUILDING MICRO-MANAGER PLUGINS";
+
+$ant ${quiet} -Dmm.architecture=${platform} -buildfile "openspim.build.xml" build-plugins || bannerdie "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(";
+
+banner "${target:+RE}BUILDING MICRO-MANAGER AUTO-FOCUS";
+
+$ant ${quiet} -Dmm.architecture=${platform} -buildfile "autofocus/build.xml" ${target:+clean }jar || bannerdie "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(";
+
+banner "STAGING OUTPUT FILES";
+
+$ant ${quiet} -Dmm.architecture=${platform} -buildfile "openspim.build.xml" stage || bannerdie "FAILED TO STAGE OUTPUT FILES! :(";
 
 banner "DONE! :D";
